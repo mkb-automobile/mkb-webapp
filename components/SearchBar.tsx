49d971a29ac7a95dd6ec9f94f2919747ae70194c
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2023-09-29 10:29:03 +0200

    add SearchBar input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
     <Image
-      src="/public/magnifying-glass.svg"
+      src="/magnifying-glass.svg"
       alt="maginifying glass"
       width={40}
       height={40}
@@ -17,6 +17,7 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 
 function SearchBar() {
   const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
   const handleSearch = () => {};
 
@@ -29,15 +30,25 @@ function SearchBar() {
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
-      <div className="search__item">
+      <div className="searchbar__item">
         <Image
-          src={"/model-ico.png"}
+          src={"/model-icon.png"}
           alt="car model"
           width={25}
           height={25}
           className="absolute w-[20px] h-[20px] ml-4"
         />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 }
