104571dce600631910637e4f897fcd551635d475
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2023-09-30 13:38:13 +0200

    edit page.tsx and add About page

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,6 @@ import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 import Catalogue from "@/pages/Catalogue";
 
-let kilometer = [];
-for (let kilometrage = 0; kilometrage <= 300000; kilometrage += 10000) {
-  console.log(kilometrage);
-  kilometer.push(kilometrage);
-}
-console.log(kilometer);
-
 export default function Home({}) {
   return (
     <main className="overflow-hidden">
