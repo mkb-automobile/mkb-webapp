a24ee1314f305a365164147695f09b06f04a130b
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2023-11-03 18:08:04 +0100

    delete Old carDetail

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 import Hero from "./Hero";
 import CustomFilter from "./CustomFilter";
 import CarCard from "./CarCard";
-import CarDetails from "./CarDetails";
 import FormContact from "./FormContact";
 
-export { Hero, CustomFilter, CarCard, CarDetails, FormContact };
+export { Hero, CustomFilter, CarCard, FormContact };
