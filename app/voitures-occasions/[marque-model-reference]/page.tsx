12de6a344756f64afb4a6911eaf67d10a9353178
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2023-11-08 18:05:28 +0100

    finish carDetail

--- app/voitures-occasions/[marque-model-reference]/page.tsx
+++ app/voitures-occasions/[marque-model-reference]/page.tsx
@@ -10,7 +10,7 @@ import { useCarContext } from "@/src/hooks/CarContext";
 import { FaRegCreditCard } from "react-icons/fa";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import { FaAward } from "react-icons/fa";
-import { BsCarFront } from "react-icons/bs";
+import { BsCarFront, BsInstagram } from "react-icons/bs";
 import { MdLocalGasStation } from "react-icons/md";
 import { GoMilestone } from "react-icons/go";
 import { TbManualGearbox } from "react-icons/tb";
@@ -19,6 +19,8 @@ import { GiCarSeat } from "react-icons/gi";
 import { PiEngineThin } from "react-icons/pi";
 import { GiCarDoor } from "react-icons/gi";
 import { FormContact } from "@/src/components";
+import { BsFacebook } from "react-icons/bs";
+import { it } from "node:test";
 
 interface PageProps {
   params: {
@@ -121,47 +123,35 @@ export default function Page({ params }: PageProps) {
     },
   ];
 
+  const reseauSocial = [
+    {
+      logo: BsFacebook,
+      titre: "Facebook",
+      href: "https://www.facebook.com/mkb.automobile",
+    },
+    {
+      logo: BsInstagram,
+      titre: "Instagram",
+      href: "https://www.instagram.com/mkbautomobile/?igshid=MWs0bTB4YW11a2JkZQ%3D%3D",
+    },
+  ];
+
   return (
     <Main>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
         <>
-          <div className="flex px-5 w-full">
-            <div className="w-3/5 p-2">
+          <div className="flex px-5 w-full max-md:flex-col">
+            <div className="w-3/5 p-2 max-md:w-full">
               <PhotoSlider car={car} />
               <div className="grid grid-cols-3 gap-2 pb-10">
                 {car?.photos[0]?.photo?.map((url, index) => (
                   <PhotoCard key={index} url={url} alt={car.title} />
                 ))}
               </div>
-              <div>
-                <div className="text-3xl font-light w-full pb-10">
-                  <h2>Critéres</h2>
-                </div>
-                <div className="bg-primary-orange-100 p-10  grid grid-cols-3  gap-5 justify-evenly rounded-xl shadow-2xl">
-                  {cardDetailsCritere.map((item, index) => {
-                    const Icon = item.logo;
-                    return (
-                      <div key={index} className="text-xl">
-                        <div className="flex items-center gap-2 ">
-                          <Icon />
-                          <span> {item.titre}</span>
-                        </div>
-                        <div className="text-orange-900">{item.value}</div>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-              <div className="py-10">
-                <h2 className="text-3xl font-light pb-5">
-                  Description & Équipements
-                </h2>
-                <ul className="text-md">{elements}</ul>
-              </div>
             </div>
-            <div className="w-1/3 pl-6">
+            <div className="w-1/3 pl-6 max-md:w-full">
               <div className=" flex flex-col justify-evenly">
                 <div className="py-5 border-b-2 border-b-black">
                   <h2 className="text-2xl font-bold">
@@ -207,8 +197,8 @@ export default function Page({ params }: PageProps) {
                     )}
                   </div>
                 </div>
-                <div className="flex justify-evenly border-b-2 border-b-black p-5">
-                  <div className="flex items-center justify-center bg-primary-orange p-5 rounded-full gap-2 shadow-xl">
+                <div className="flex justify-evenly border-b-2 border-b-black p-5 max-sm:flex-col max-sm:gap-3 max-sm:p-10">
+                  <div className="flex items-center justify-center bg-primary-orange p-5 rounded-full gap-2 shadow-xl max-sm:p-3">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
@@ -226,7 +216,7 @@ export default function Page({ params }: PageProps) {
                       href={"tel:+33188830917"}
                     />
                   </div>
-                  <div className="flex items-center justify-center bg-primary-orange p-5 rounded-full gap-2 shadow-xl">
+                  <div className="flex items-center justify-center bg-primary-orange p-5 rounded-full gap-2 shadow-xl max-sm:p-3">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
@@ -280,32 +270,90 @@ export default function Page({ params }: PageProps) {
               </div>
             </div>
           </div>
+          <div className="flex flex-row-reverse justify-evenly px-5 max-md:flex-col">
+            <div className="w-1/2 max-md:w-full py-5">
+              <div className="text-3xl font-light w-full pb-10">
+                <h2>Critéres</h2>
+              </div>
+              <div className="bg-primary-orange-100 p-10  grid grid-cols-3  lg:grid-cols-2 max-sm:flex flex-col items-center gap-5 justify-evenly rounded-xl shadow-2xl">
+                {cardDetailsCritere.map((item, index) => {
+                  const Icon = item.logo;
+                  return (
+                    <div
+                      key={index}
+                      className="flex flex-col items-center text-xl"
+                    >
+                      <div className="flex items-center gap-2 ">
+                        <Icon />
+                        <span> {item.titre}</span>
+                      </div>
+                      <div className="text-orange-900">{item.value}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="w-1/2 max-md:w-full py-5">
+              <h2 className="text-3xl font-light pb-5">
+                Description & Équipements
+              </h2>
+              <ul className="grid grid-cols-2 text-md">{elements}</ul>
+            </div>
+          </div>
           <div className="w-full flex flex-col items-center py-10 bg-primary-orange-100 gap-6 shadow-xl">
             <div>
               <h1 className="text-center font-semibold text-3xl pb-5">
                 Ce modèle vous intéresse ?
                 <span style={{ color: "#ff9f1c" }}> Contactez-nous</span>
               </h1>
-              <p>
+              <p className="text-center pb-5">
                 Nos experts vous accompagnenent pour votre projet d'achat
                 automobile.
                 <br />
-                Nous somme à cotre écoute pour vous conseiller et vous aider à
-                trouver le véhicule qui vous correspond.
+                Nous somme à cotre écoute.
               </p>
-              <p className="text-blod">
+              <p className="text-center font-bold">
                 Écrivez-nous, nous vous répondrons dans les plus brefs délais.
               </p>
             </div>
-            <div className="">
-              <FormContact />
-            </div>
-            <div>
-              <p>Vous preferez nous contacter par téléphone ? </p>
-              <CustomLink
-                title="Appeler notre équipe"
-                href="tel:+33188830917"
-              />
+            <div className="w-full flex justify-evenly max-md:flex-col">
+              <div className="">
+                <FormContact />
+              </div>
+              <div className="flex flex-col  items-center justify-center">
+                <p>Vous preferez nous contacter par téléphone ? </p>
+                <div className="py-5 border-b-2 border-b-black">
+                  <div className="flex justify-center bg-primary-orange p-5 rounded-full gap-2 shadow-xl">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <CustomLink
+                      title="Parler a notre équipe"
+                      href={"tel:+33188830917"}
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-10 py-5">
+                  {reseauSocial.map((item, index) => (
+                    <div
+                      key={index}
+                      className="flex items-center justify-center bg-primary-orange p-5 rounded-full gap-2 shadow-xl"
+                    >
+                      <item.logo className="w-6 h-6" />
+                      <CustomLink title={item.titre} href={item.href} />
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </>
--- src/types/index.ts
+++ src/types/index.ts
@@ -12,7 +12,7 @@ export interface CustomButtonProps {
 }
 
 export interface CustomLinkProps {
-  title: string;
+  title?: string;
   href: string;
   containerStyles?: string;
 }
