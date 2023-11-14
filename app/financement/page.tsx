8bdd0b93023db0ff7fbe9b019ac683192ad91af0
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2023-11-14 09:36:34 +0100

    begin fincing page

--- app/financement/page.tsx
+++ app/financement/page.tsx
@@ -4,7 +4,30 @@ import React from "react";
 const Financing = () => {
   return (
     <Main>
-      <div>Ceci est la page de financement</div>
+      <section>
+        <div>
+          <h1 className="">
+            <span style={{ color: "#ff9f1c" }}>Financez votre véhicule</span> en
+            ligne
+          </h1>
+          <p>
+            <strong>
+              Vous cherchez un financement pour votre nouveau véhicule ?
+            </strong>
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <div>
+            <h2 className="">Paiement en plusieurs fois</h2>
+          </div>
+          <div>
+            <h2 className="">Crédit financement</h2>
+          </div>
+        </div>
+      </section>
     </Main>
   );
 };
